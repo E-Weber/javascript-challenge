c2ff40d10642474d77de5f5487f5a152e2a87b32
E-Weber <ewebersms@gmail.com>
2021-04-26 19:57:50 -0500

    beautiful table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,7 +3,15 @@ var tableData = data;
 console.log(tableData)
 // add data
 var tbody = d3.select("tbody");
-// add tr
-var tr = tbody.append("tr");
-//append
-tr.append("td").text()
+console.log(data);
+// loop through data with forEach
+data.forEach(sighting => {
+    console.log(sighting);
+    // append
+    var trow = tbody.append("tr");
+    Object.entries(sighting).forEach(([key, value]) => {
+        console.log(key, value);
+        var tcell = trow.append("td");
+        tcell.text(value)
+    })
+})
