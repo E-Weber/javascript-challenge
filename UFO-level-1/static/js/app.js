2df367be439a9320800ce17479c6f736ff41652f
E-Weber <ewebersms@gmail.com>
2021-04-26 22:47:18 -0500

    don't know

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -20,22 +20,28 @@ data.forEach(sighting => {
 })
 
 // select button form input
-var click = d3.select("#filter-btn");
+var button = d3.select("#filter-btn");
 var form = d3.select("form");
-// form and click on
-form.on('submit', runEnter);
-click.on("click", runEnter);
-
-function runEnter() {
+// form and button on
+//form.on('submit', runEnter);
+button.on("click", function (runEnter) {
 
     //prevent function
     d3.runEnter.preventDefault();
     var inputElement = d3.select("#datetime");
     var inputValue = inputElement.property("value")
 
-    var filterData = data.filter(sighting => sighting.datetime === inputValue);
+    var filterData = tableData.filter(sTime => ((Date(sTime.datetime)) > (Date(inputValue))));
     console.log(filterData)
 
-    List.html("");
+    //tbody.html("");
+    var table = d3.select("table");
+    table.html = ("<tbody></tbody>");
 
-};
+    filterData.forEach(function (dfilter) {
+        var trow = d3.select("tbody").append("tr");
+        Object.entries(dfilter).forEach(([key, value]) => {
+            trow.append("td").text(value);
+        });
+    })
+});
