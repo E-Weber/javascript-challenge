ae2dc2311c0c6bb1eeaeec4c6e151004ec2309c6
E-Weber <ewebersms@gmail.com>
2021-04-26 21:21:43 -0500

    working on button

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -19,5 +19,23 @@ data.forEach(sighting => {
     })
 })
 
-// reference button
+// select button form input
 var click = d3.select("#filter-btn");
+var form = d3.select("form");
+// form and click on
+form.on('submit', runEnter);
+click.on("click", runEnter);
+
+function runEnter() {
+
+    //prevent function
+    d3.runEnter.preventDefault();
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value")
+
+    var filterData = data.filter(sighting => sighting.datetime === inputValue);
+    console.log(filterData)
+
+    List.html("");
+
+};
