c3a535b636c5e1fac14b0eb80b03a22783f99af7
E-Weber <ewebersms@gmail.com>
2021-04-26 19:22:25 -0500

    tbody

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,9 @@
 // from data.js
 var tableData = data;
-
-// YOUR CODE HERE!
+console.log(tableData)
+// add data
+var tbody = d3.select("tbody");
+// add tr
+var tr = tbody.append("tr");
+//append
+tr.append("td").text()
